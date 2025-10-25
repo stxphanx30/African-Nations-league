033838ddbf66d6f08951780306d01f1741a739f9
stxphanx30 <ST10158209@vcconnect.edu.za>
2025-10-25 18:12:20 +0200

    Add project files.

---
+++ African Nations league/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using African_Nations_league.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace African_Nations_league.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ African Nations league/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace African_Nations_league.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
