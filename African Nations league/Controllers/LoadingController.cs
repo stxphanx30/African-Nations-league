4fc295db52c62ef53dea12d0f31fbbac9b6e107d
stxphanx30 <ST10158209@vcconnect.edu.za>
2025-10-25 18:20:34 +0200

    Adding landing screen

---
+++ African Nations league/Controllers/LoadingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace African_Nations_league.Controllers
+{
+    public class LoadingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
